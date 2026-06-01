b8301a66764f9ef7d71f6307445cd706baa8bfed
Innovación y Desarrollo - HUV <innovacionydesarrollo@correohuv.gov.co>
2026-06-01 11:31:18 -0500

    cambios

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Confiar en todos los proxies (Cloudflare, Nginx, Load Balancers, etc)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
